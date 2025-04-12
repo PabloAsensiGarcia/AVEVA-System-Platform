610235abe92b3ebfff5790d28974564933202c4c
Pablo Asensi Garcia <111115373+PabloAsensiGarcia@users.noreply.github.com>
2025-04-12 20:34:36 +0100

    Delete bitmaskingDataStructures.vb

--- bitmaskingDataStructures.vb
+++
@@ -1,9 +0,0 @@
-' Extracts and evaluates Bit 0 from the input word (Me.stsWord).
-' If Bit 0 is set (i.e., not equal to 0), the corresponding alarm attribute (Me.almAlarm001) is triggered.
-' Otherwise, the alarm is cleared.
-
-if ((Me.stsWord & (1 SHL 0)) <> 0) then
-    Me.almXXX = true;
-else
-    Me.almXXX = false;
-endif;
